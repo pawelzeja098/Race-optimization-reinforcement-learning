a2c3149fca229544cd03239d70cada33a11dc19d
Pawel <pawel.zeja@infokam.kielce.pl>
2025-09-22 14:23:14 +0200

    training state predictor

--- envs/filtr_json_from_race.py
+++ envs/filtr_json_from_race.py
@@ -1,9 +1,15 @@
 # scoring_file_raw = "data/raw_races/scoring_data.json"
 # telem_file_raw = "data/raw_races/telemetry_data.json"
-
-scoring_file_filtered = "data/filtered_races/scoring_data.json"
-telem_file_filtered = "data/filtered_races/telemetry_data.json"
+import numpy as np
+import sqlite3
 import json
+import numpy as np
+# from generate_actions_BC import generate_actions_BC
+# from generate_state_BC import extract_state
+
+# scoring_file_filtered = "data/filtered_races/scoring_data.json"
+# telem_file_filtered = "data/filtered_races/telemetry_data.json"
+
 
 def filtr_json_files(telem_file_raw, scoring_file_raw):
     data_telem = json.load(open(telem_file_raw ))
@@ -22,47 +28,88 @@ def filtr_json_files(telem_file_raw, scoring_file_raw):
 
     # with open("data/telemetry_data_filtered.json", "w") as f:
     #     json.dump(filtered_data, f, indent=2)
+    scoring_records_len = 0
+    data_before_start = 0
+    start_flag = False
 
     for entry in raw_data_scoring:
         # for vehicle in raw_data_scoring.get("mVehicles", []):
+        start = entry["mStartLight"]
+        if start == 0 and not start_flag:
+            data_before_start += 1
+            start_flag = True
+            continue
 
         #     if vehicle.get("mIsPlayer"):
-        vehicle = entry.get("mVehicles", [])
+        vehicle = entry.get("mVehicles")
 
-        wanted_weather_keys = ["mRaining","mAmbientTemp","mTrackTemp","mEndET", "mCurrentET"]
+        wanted_weather_keys = ["mRaining","mAmbientTemp","mTrackTemp","mEndET", "mCurrentET","mAvgPathWetness"]
         subset_weather = {k: entry.get(k) for k in wanted_weather_keys}
+        subset_weather["mTotalLapDistance"] = entry["mLapDist"]
         
         
-        vehicle_sector = vehicle.get("mSector", -1)
-        #save only if sector changed
-        if vehicle_sector == curr_sector:
-            print(f"curr_sector: {curr_sector}, vehicle_sector: {vehicle_sector}")
-            continue
-        print(f"Sector changed: {curr_sector} -> {vehicle_sector}")
+        vehicle_sector = vehicle[0]["mSector"]
+        
         curr_sector = vehicle_sector
-        wanted_keys = ["mLastLapTime","mBestLapTime","mCurrLapTime","mNumPitstops","mNumPenalties","mInPits"]
-        subset_scoring_vehicle = {k: vehicle.get(k) for k in wanted_keys}
+        wanted_keys = ["mLastLapTime","mBestLapTime","mCurrLapTime","mNumPitstops","mNumPenalties","mInPits","mFinishStatus","mLapDist","mSector","mTotalLaps"]
+        if vehicle[0]["mFinishStatus"] == 1:
+            subset_scoring_vehicle = {k: vehicle[0].get(k) for k in wanted_keys}
+
+        # print(json.dumps(vehicle, indent=2))
+        # scoring_records.append(data)
+            filtered_data_scoring.append({**subset_scoring_vehicle, **subset_weather})
+            scoring_records_len += 1
+
+            break
+
+        subset_scoring_vehicle = {k: vehicle[0].get(k) for k in wanted_keys}
 
         # print(json.dumps(vehicle, indent=2))
         # scoring_records.append(data)
         filtered_data_scoring.append({**subset_scoring_vehicle, **subset_weather})
-
+        scoring_records_len += 1
+    
+    telemetry_records_len = 0
+    count_before_start = 0
     for entry in raw_data_telemetry:
+        if count_before_start < data_before_start:
+            count_before_start += 1
+            continue
+        if telemetry_records_len >= scoring_records_len:
+            break
+        wanted_keys = ["mFuel", "mFuelCapacity","mWheel","mDentSeverity","mFrontTireCompoundIndex","mCurrentSector","mLapNumber","multiplier"]
 
-        wanted_keys = ["mFuel", "mFuelCapacity","mWheel","mDentSeverity","mFrontTireCompoundIndex","mCurrentSector","mLapNumber"]
-
-
+        avg_temp = 0
         subset = {k: entry.get(k) for k in wanted_keys}
+        # for wheel in subset["mWheel"]:
+        #     for temp in wheel.get("mTemperature", 0):
+        #         avg_temp += temp
+        #     avg_temp /= len(wheel.get("mTemperature", []))
 
-        subset["mWheel"] = [
-                {
-                    "mWear": wheel.get("mWear"),
-                    "mBrakeTemp": wheel.get("mBrakeTemp"),
-                    "mTemperature": wheel.get("mTemperature"),
-                }
-                for wheel in entry.get("mWheel", [])
-            ]
+       #Mean values for wheels
+
+        # wheels = entry.get("mWheel", [])
+        # avg_wear = 0
+        # avg_brake_temp = 0
+        # avg_temp = 0
+        # for wheel in wheels:
+        #     avg_wear += wheel.get("mWear", 0)
+        #     # avg_brake_temp += wheel.get("mBrakeTemp", 0)
+        #     # avg_temp += wheel.get("mTemperature", 0)
+        #     for temp in wheel.get("mTemperature", 0):
+        #         avg_temp += temp
+        #     avg_temp /= len(wheel.get("mTemperature", []))
+        # avg_wear /= len(wheels)
+        # # avg_brake_temp /= len(wheels)
+        # avg_temp /= len(wheels)
+
+        # subset["mWheel"] = {
+        #     "mWear": avg_wear,
+        #     "mBrakeTemp": avg_brake_temp,
+        #     "mTemperature": avg_temp,
+        # }
         filtered_data_telemetry.append(subset)
+        telemetry_records_len += 1
         # print(json.dumps(subset, indent=2))
     
 
@@ -71,3 +118,163 @@ def filtr_json_files(telem_file_raw, scoring_file_raw):
     #     json.dump(filtered_data_telemetry, f, indent=2)
     # with open(scoring_file_filtered, "w") as f:
     #     json.dump(filtered_data_scoring, f, indent=2)
+
+
+def extract_state(telem_file_raw, scoring_file_raw):
+        filtered_data_telemetry, filtered_data_scoring = filtr_json_files(telem_file_raw,scoring_file_raw)
+        
+        data_state = []
+        
+        scoring_all = filtered_data_scoring
+        telemetry_all = filtered_data_telemetry
+        endET = -1
+
+        for i in range(len(telemetry_all)):
+            telemetry = telemetry_all[i]
+            scoring = scoring_all[i]
+          
+            
+            last_lap = scoring["mLastLapTime"]
+            best_lap = scoring["mBestLapTime"]
+            has_last_lap = 1.0 if last_lap > 0 else 0.0
+            if last_lap <= 0.0:
+                last_lap = 0  # placeholder
+                best_lap = 0
+            
+            if scoring["mEndET"] < 0:
+                endET = scoring_all[i+4]["mEndET"]
+
+            else:
+                endET = scoring["mEndET"]    
+                
+
+            data_state_per = [
+                #dane do przewidzenia
+                #dane ciągłe
+                
+                # scoring["mCurrLapTime"],
+                
+                round(scoring["mLapDist"]/scoring["mTotalLapDistance"],5),
+                round(scoring["mCurrentET"]/endET,5),
+
+                round(telemetry["mFuel"]/telemetry["mFuelCapacity"],5),
+                round(telemetry['mWheel'][0]['mWear'], 5),  # Average wear across all four tires
+                round(telemetry["mWheel"][1]["mWear"], 5),
+                round(telemetry["mWheel"][2]["mWear"], 5),
+                round(telemetry["mWheel"][3]["mWear"], 5),
+                round(sum(telemetry["mWheel"][0]["mTemperature"])/len(telemetry["mWheel"][0]["mTemperature"]), 5),
+                round(sum(telemetry["mWheel"][1]["mTemperature"])/len(telemetry["mWheel"][1]["mTemperature"]), 5),
+                round(sum(telemetry["mWheel"][2]["mTemperature"])/len(telemetry["mWheel"][2]["mTemperature"]), 5),
+                round(sum(telemetry["mWheel"][3]["mTemperature"])/len(telemetry["mWheel"][3]["mTemperature"]), 5),
+                scoring["mAvgPathWetness"],
+
+
+                #dane dyskretne
+                
+                scoring["mNumPenalties"],
+                
+                
+                telemetry["mDentSeverity"][0],  # Not defined which part of the car this refers to each index
+                telemetry["mDentSeverity"][1],
+                telemetry["mDentSeverity"][2], 
+                telemetry["mDentSeverity"][3],
+                telemetry["mDentSeverity"][4],
+                telemetry["mDentSeverity"][5],
+                telemetry["mDentSeverity"][6], 
+                telemetry["mDentSeverity"][7],
+
+                #dane pomocnicze
+                
+
+                has_last_lap,
+                scoring["mFinishStatus"],
+                scoring["mTotalLaps"],
+                scoring["mSector"],
+                scoring["mNumPitstops"],
+                int(scoring["mInPits"]),
+                telemetry["mFrontTireCompoundIndex"],
+                scoring["mRaining"],
+                round(scoring["mAmbientTemp"], 2),
+                round(scoring["mTrackTemp"], 2),
+                
+                telemetry["multiplier"],
+
+                last_lap,
+                best_lap,
+                
+                
+        ]
+            data_state.append(data_state_per)
+
+       
+        # with open("data/state_data.json", "w") as file:
+        #     json.dump(data_state, file, indent=2)
+
+        return data_state
+
+
+
+def save_to_db(telem_file_raw, scoring_file_raw):
+    # przykładowe stany i akcje
+    states = extract_state(telem_file_raw, scoring_file_raw)
+
+    # zamieniamy listy na JSON
+    states_json = json.dumps(states)
+   
+    # tworzymy bazę / tabelę
+    conn = sqlite3.connect("data/db_states_for_regress/race_data_states.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS races (
+        race_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        states_json TEXT
+        
+    )
+    """)
+
+    # zapisujemy rekord, nie podajemy race_id, SQLite automatycznie nada
+    cursor.execute("""
+    INSERT INTO races (states_json)
+    VALUES (?)
+""", (states_json,)) 
+
+    conn.commit()
+    conn.close()
+
+
+
+
+def load_from_db():
+    conn = sqlite3.connect("data/db_states_for_regress/race_data_states.db")
+    cursor = conn.cursor()
+
+    # wybierz wszystkie rekordy z tabeli
+    cursor.execute("SELECT * FROM races")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        race_id, states_json = row
+        
+        
+        # zamiana JSON z powrotem na listę
+        states = json.loads(states_json)
+        
+        
+        print(f"Number of states: {len(states)}")
+        print("Przykładowy state:", states)
+       
+        print("-"*40)
+
+    conn.close()
+
+
+
+def delete_from_db(race_id):
+    conn = sqlite3.connect("data/db_states_for_regress/race_data_states.db")
+    cursor = conn.cursor()
+
+    race_id_to_delete = 1
+    cursor.execute("DELETE FROM races WHERE race_id = ?", (race_id_to_delete,))
+    conn.commit()
+    conn.close()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
 from telemetry.LMU_plugin import collect_telemetry
-from envs.filtr_json_from_race import filtr_json_files
+from envs.filtr_json_from_race import save_to_db, load_from_db, delete_from_db
 # from envs.racing_env import RacingEnv
 # from ai.BCmodel import BCModel, load_data, save_model
 import torch
@@ -7,11 +8,53 @@ import torch
 import numpy as np
 import time
 import os
+from pathlib import Path
 
 
 if __name__ == "__main__":
-    usage_multiplier = 3.0  # Ustawienia wyścigu
-    collect_telemetry(usage_multiplier)
+    # usage_multiplier = 3.0  # Ustawienia wyścigu
+    # collect_telemetry(usage_multiplier)
+    
+
+    data_dir = Path(r"E:/pracadyp/Race-optimization-reinforcement-learning/data/raw_races")
+
+    for telem_path in data_dir.glob("telemetry_data_*.json"):
+        # Tworzymy odpowiadającą nazwę scoring
+        scoring_path = data_dir / telem_path.name.replace("telemetry", "scoring")
+        if scoring_path.exists():
+            save_to_db(str(telem_path), str(scoring_path))
+
+
+    # telem_files_raw = [r"E:/pracadyp/Race-optimization-reinforcement-learning/data/raw_races/telemetry_data_x330min_goodver.json",r"E:/pracadyp/Race-optimization-reinforcement-learning/data/raw_races/telemetry_data_x330min_goodver.json", r"E:/pracadyp/Race-optimization-reinforcement-learning/data/raw_races/telemetry_data_x310min.json"]
+    # scoring_files_raw = [r"E:/pracadyp/Race-optimization-reinforcement-learning/data/raw_races/scoring_data_x330min_goodver.json",r"E:/pracadyp/Race-optimization-reinforcement-learning/data/raw_races/scoring_data_x330min_goodver.json", r"E:/pracadyp/Race-optimization-reinforcement-learning/data/raw_races/scoring_data_x310min.json"]
+    # for telem_file_raw, scoring_file_raw in zip(telem_files_raw, scoring_files_raw):
+    #     save_to_db(telem_file_raw, scoring_file_raw)
+    
+    load_from_db()
 
     # filtr_json_files()
     
+    # telemetry = "data/raw_races/telemetry_data.json"
+    # with open(telemetry, "r", encoding="utf-8") as f:
+    #     data = json.load(f)
+
+    # # jeśli plik zawiera listę obiektów:
+    # if isinstance(data, list):
+    #     for obj in data:
+    #         if "multiplier" in obj:
+    #             obj["multiplier"] = 2.0
+    #         if "usage multiplier" in obj:
+    #             obj["usage multiplier"] = 2.0
+
+    # # jeśli plik zawiera pojedynczy obiekt:
+    # elif isinstance(data, dict):
+    #     if "multiplier" in data:
+    #         data["multiplier"] = 2.0
+    #     if "usage multiplier" in data:
+    #         data["usage multiplier"] = 2.0
+
+    # # zapisujemy z powrotem w to samo miejsce
+    # with open(telemetry, "w", encoding="utf-8") as f:
+    #     json.dump(data, f, indent=2, ensure_ascii=False)
+
+    # print("✅ Plik został zaktualizowany (multiplier = 2.0)")
